3ba1c4fb5b56b66a0f17606e7aa16304b93870cb
DanGarther <47385923+DanGarther@users.noreply.github.com>
2019-02-07 12:28:00 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,55 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+
+var num1    
+var num2
+
 function sumar()
-{	
+{
+  
+    num1 = document.getElementById("numeroUno").value
+    num1 = parseInt(num1)
+    
+    num2 = document.getElementById("numeroDos").value
+    num2 = parseInt(num2)
+    alert(num1 + num2)	
 	
 }
 
 function restar()
 {
+    num1 = document.getElementById("numeroUno").value
+
+    
+    num2 = document.getElementById("numeroDos").value
+
+    alert(num1 - num2)
 	
 }
 
 function multiplicar()
 { 
 	
+    
+    num1 = document.getElementById("numeroUno").value
+ 
+    
+    num2 = document.getElementById("numeroDos").value
+   
+    alert(num1 * num2)
 }
 
 function dividir()
 {
 	
+    
+    num1 = document.getElementById("numeroUno").value
+    
+    
+    num2 = document.getElementById("numeroDos").value
+    
+    alert(num1 / num2)
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,28 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+var num1
+var num2
+var num3
+var num4
+
+num1 = document.getElementById("sueldo").value
+num1 = parseInt(num1)
+
+num2 = num1 * 10
+num2 = parseInt(num2)
+
+num3 = num2 / 100
+num3 = parseInt(num3)
+
+num4 = num1 + num3
+num4 = parseInt(num4)
+
+
+
+document.getElementById("resultado").value = num4
+
+alert(num3)
 
 	
 }
