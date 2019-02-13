a449aef93538987d4b8d72b7176f250ad38461e4
DanGarther <47385923+DanGarther@users.noreply.github.com>
2019-02-13 12:23:28 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,17 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad = document.getElementById("edad").value
+
+if(edad == 15){
+
+    alert("Niña Bonita")
+
+}
+
+alert("Fin de la funcion")
 
 
 }//FIN DE LA FUNCIÓN
